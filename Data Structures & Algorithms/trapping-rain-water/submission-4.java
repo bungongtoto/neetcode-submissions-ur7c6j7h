359671f07fdcb40c933f00c5b68ae2ac2c1457e1
BUNGONG KINGSLEY NDZI <118504455+bungongtoto@users.noreply.github.com>
2026-09-02 22:27:59 +0100

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int l = 0 , r = n - 1;
+
+        int total = 0;
+        int leftMax = 0, rightMax = 0;
+
+        while (l < r){
+            if (height[l] < height[r]){
+                if (height[l] > leftMax){
+                    leftMax = height[l];
+                }else {
+                    total += leftMax - height[l];
+                }
+                l++;
+            }else {
+                 if (height[r] > rightMax){
+                    rightMax = height[r];
+                }else {
+                    total += rightMax - height[r];
+                }
+                r--;
+            }
+            
+        }
+
+        return total;
+    }
+}
