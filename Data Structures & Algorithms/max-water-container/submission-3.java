cad2025f3bb55d0904cf101035805559670c11c8
BUNGONG KINGSLEY NDZI <118504455+bungongtoto@users.noreply.github.com>
2026-09-02 21:52:41 +0100

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int n = heights.length;
+        int l = 0, r = n - 1;
+
+        int maxArea = 0;
+
+        while (l < r){
+            int area =  Math.min(heights[l], heights[r]) * (r - l);
+            maxArea = Math.max(maxArea,area);
+
+            if (heights[l] > heights[r]){
+                r--;
+            }else {
+                l++;
+            }
+        }
+
+        return maxArea;
+    }
+}
