a6212a4573eea44267341488e5e5fd8aa6e4058c
BUNGONG KINGSLEY NDZI <118504455+bungongtoto@users.noreply.github.com>
2026-09-16 12:50:24 +0100

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+
+        int l = 0, r = ROWS * COLS - 1;
+
+        while (l <= r){
+            int mid = ((r - l) / 2) + l;
+
+
+            int row = mid / COLS, col = mid % COLS;
+
+            if (matrix[row][col] > target){
+                r = mid - 1;
+            }else if (matrix[row][col] < target){
+                l = mid + 1;
+            }else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
